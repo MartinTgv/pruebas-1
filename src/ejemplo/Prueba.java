0124ea2db58328931076cc27c037f8d8856d523f
mdelrio <mdelrio@tgv.com.ar>
2018-06-27 12:46:19 -0300

    prueba de comiteo

--- src/ejemplo/Prueba.java
+++ src/ejemplo/Prueba.java
@@ -3,7 +3,7 @@ package ejemplo;
 public class Prueba {
 
 	public static void main(String[] args) {
-	System.out.println("prueba");
+	System.out.println("prueba"); System.out.println("cambio1");
 	}
 
 }
