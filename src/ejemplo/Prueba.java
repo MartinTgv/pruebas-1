cc9786fee5bef4783175770bf866fc3a33b0b7be
mdelrio <mdelrio@tgv.com.ar>
2018-06-27 16:26:38 -0300

    prueba2

--- src/ejemplo/Prueba.java
+++ src/ejemplo/Prueba.java
@@ -3,7 +3,8 @@ package ejemplo;
 public class Prueba {
 
 	public static void main(String[] args) {
-	System.out.println("prueba"); System.out.println("cambio1");
+	System.out.println("prueba");  
+	System.out.println("cambio1");
 	}
 
 }
